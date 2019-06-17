b46a4b32343a002caa320d09076e404b1c3d41bf
Anton Surganov <antonsurganov@yandex.ru>
2019-06-18 00:37:23 +0300

    corrections 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+  has_many :created_tests, class_name: 'Test'
+  
   def passed_tests(level)
     Test.where(level: level).joins("INNER JOIN tests_users ON tests.id = tests_users.test_id").where("tests_users.user_id = ?", self.id)
   end
-
-  has_and_belongs_to_many :tests
-  has_many :created_tests, inverse_of: 'author'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_173039) do
+ActiveRecord::Schema.define(version: 2019_06_17_213434) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_06_15_173039) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id", null: false
+    t.integer "user_id"
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
